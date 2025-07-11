37337debcd386157d31e9b9f9b685d6ed4f7c958
Sayan Saha <sayansaha5161@gmail.com>
2025-07-11 22:50:45 +0530

    Add files via upload

---
+++ Assinement.py
@@ -0,0 +1,27 @@
+#('Enter your First Name: Sayan')
+#('Enter your Second Name: Saha ')
+#Hello, Sayan Saha! Welcome to the Python Program
+
+First_Name=input('Enter your First Name: Sayan')
+Second_Name=input('Enter your Second Name: Saha')
+print('Hallo, Sayan saha! Welcome tothe Python Program')
+
+
+ #Takes two numbers as input from the user.
+#2.  Performs the basic mathematical operations on these two numbers:
+#o	Addition
+#o	Subtraction
+#o	Multiplication
+#o	Division
+#3.  Displays the results of each operation on the screen.
+
+
+a = int(input('Enter The First Number: '))
+b = int(input('Enter The Second Number: '))
+
+print("Number a is: ", a)
+print("Number b is: ", b)
+print("Addition is: ", a + b)
+print("Subtraction is: ", a - b)
+print("Multiplikation is: ", a * b)
+print("Divition is: ", a / b)
